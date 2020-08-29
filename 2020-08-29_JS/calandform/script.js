1b8a0f34e54e7210e9921d1e332eceea261ae968
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-08-29 13:19:10 -0500

    Add js

---
+++ 2020-08-29_JS/calandform/script.js
@@ -0,0 +1,4 @@
+const inputName = document.getElementById('inputName')
+const inputApellido = document.getElementById('inputApellido')
+const inputCorreo = document.getElementById('inputCorreo')
+const inputCpassword = document.getElementById('inputCpassword')
